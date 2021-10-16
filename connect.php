d3f1d74550eaad033c686e85745326f29638d24c
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2021-10-16 12:30:13 +0200

    ADDED actual create table queries

--- connect.php
+++ connect.php
@@ -1,15 +1,39 @@
 <?php
 
 // Create connection
-$pdo = new PDO('sqlite:db/elephpant.db');
-
-function tableExist(object $conn, string $table): bool
-{
-    try {
-        $tryConn = $conn->query("SELECT 1 FROM $table");
-    } catch (Exception $error) {
-        return false;
-    }
-    return $tryConn !== false;
+try {
+    $db = new PDO('sqlite:db/elephpant.db');
+} catch (Exception $err) {
+    "could not find database\n$err";
 }
-echo tableExist($pdo, 'test') ? "succes!" : "no #errorgang";
+
+//creates table
+function createTables(object $pdo): void
+{
+    $createTable = $pdo->exec(
+        'CREATE TABLE IF NOT EXISTS items (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(30)
+            );'
+    );
+    $createTable = $pdo->exec(
+        'CREATE TABLE IF NOT EXISTS locations (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(30)
+            );'
+    );
+    $createTable = $pdo->exec(
+        'CREATE TABLE IF NOT EXISTS item_location (
+                item_id INTEGER,
+                location_id INTEGER
+            );'
+    );
+}
+
+createTables($db);
+//select from table as associative
+// $query = $db->query('SELECT * FROM people');
+// $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+// foreach ($rows as $row) {
+//     echo $row['name'] . "<br>";
+// }

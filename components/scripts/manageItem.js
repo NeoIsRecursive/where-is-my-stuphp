88af4f02913fd55133a3e972610195a432aedabf
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2021-10-28 14:31:32 +0200

    ADDED js function to make update work and show inputs for it

--- components/scripts/manageItem.js
+++ components/scripts/manageItem.js
@@ -3,11 +3,24 @@ function showItem(itemInfo) {
     box.innerHTML = '';
     let info = document.createElement('p');
     console.log(itemInfo);
-    info.innerText = `name: ${itemInfo.item}. Location: ${itemInfo.location}. amount: ${itemInfo.amount}`;
-    removeBtn = document.createElement('button');
+    info.innerText = `name: ${itemInfo.item}. Location: ${itemInfo.location}. amount:`;
     info.id = "item" + itemInfo.id;
+
+    let removeBtn = document.createElement('button');
     removeBtn.innerText = 'remove';
     removeBtn.setAttribute('onclick', 'remove(' + itemInfo.id + ')');
+
+    let amount = document.createElement('input');
+    amount.id = 'amount';
+    amount.type = 'number';
+    amount.value = itemInfo.amount;
+
+    let changeAmount = document.createElement('button');
+    changeAmount.innerText = 'update';
+    changeAmount.setAttribute('onclick', 'updateAmount(' + itemInfo.id + ')');
+
+    info.appendChild(amount);
+    info.appendChild(changeAmount);
     info.appendChild(removeBtn);
     box.appendChild(info);
 }
@@ -24,5 +37,14 @@ function remove(id) {
         .then(data => {
             console.log("removed " + data + " lines");
             document.getElementById('item' + id).remove();
+        });
+}
+
+function updateAmount(id) {
+    let amount = document.getElementById('amount').value;
+    fetch('api/updateAmount.php?id=' + id + '&amount=' + amount)
+        .then(response => response.json())
+        .then(data => {
+            console.log("changed lines: " + data);
         });
 }

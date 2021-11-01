c5352127b0f5e934e7cecdc128e9b0ac8b595bc8
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2021-11-01 11:40:32 +0100

    FIXED so inserts cant be empty

--- components/api/addLocation.php
+++ components/api/addLocation.php
@@ -2,15 +2,20 @@
 
 require_once __DIR__ . '/classpath.php';
 require_once $path . '/classes/class-crud.php';
-
-$locationName = $_REQUEST['name'];
-
-$pdo = new crud;
-try {
-    $location = $pdo->newLocation($locationName);
-} catch (PDOException $err) {
-    if ($err->getCode() != 23000) {
-        die(var_dump($err->errorInfo));
+if (isset($_REQUEST['name'])) {
+    $locationName = $_REQUEST['name'];
+    if (trim($locationName) === '') {
+        echo '{ "error" :"you didnt insert anything"}';
+        die();
+    } else {
+        $pdo = new crud;
+        try {
+            $location = $pdo->newLocation($locationName);
+        } catch (PDOException $err) {
+            if ($err->getCode() != 23000) {
+                die(var_dump($err->errorInfo));
+            }
+        }
+        echo $location;
     }
 }
-echo $location;
--- components/api/addToList.php
+++ components/api/addToList.php
@@ -3,16 +3,22 @@
 require_once __DIR__ . '/classpath.php';
 require_once $path . '/classes/class-crud.php';
 
-$locationId = $_REQUEST['locationId'];
-$itemName = $_REQUEST['name'];
-
-$pdo = new crud;
-try {
-    $item = $pdo->newItem($itemName);
-} catch (PDOException $err) {
-    if ($err->getCode() != 23000) {
-        die(var_dump($err->errorInfo));
+if (isset($_REQUEST['name'], $_REQUEST['locationId'])) {
+    $locationId = $_REQUEST['locationId'];
+    $itemName = $_REQUEST['name'];
+    if (trim($itemName) === '') {
+        echo '{ "error" :"you didnt insert anything"}';
+        die();
+    } else {
+        $pdo = new crud;
+        try {
+            $item = $pdo->newItem($itemName);
+        } catch (PDOException $err) {
+            if ($err->getCode() != 23000) {
+                die(var_dump($err->errorInfo));
+            }
+        }
+        $add = $pdo->addToLocation($itemName, $locationId);
+        echo $add;
     }
 }
-$add = $pdo->addToLocation($itemName, $locationId);
-echo $add;
